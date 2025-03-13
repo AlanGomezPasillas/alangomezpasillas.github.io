309ea4bb18311b9ffd91dfe5418e1cfb183763a5
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-03-13 22:40:07 +0000

    finished and corrected language detection

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
-let la = navigator.language.substring(0,2);
-
-function checkLanguage(){
-  if(la=="es"){
-    changeLanguage(1);
-  }else{
-    changeLanguage(0);
-  }
-}
+let la = (navigator.language.substring(0,2) == "es") ? 1 : 0;
+changeLanguage(la);
 
 function changeLanguage(lan){
   if(lan==1){
@@ -21,23 +14,23 @@ function changeLanguage(lan){
 
 function changeSection(sec){
   if(sec==1){
-    document.getElementsByClassName('home').item(1).style.display='block';
+    document.getElementsByClassName('home').item(la).style.display='block';
   }else{
-    document.getElementsByClassName('home').item(1).style.display='none';
+    document.getElementsByClassName('home').item(la).style.display='none';
   }
   if(sec==2){
-    document.getElementsByClassName('music').item(1).style.display='block';
+    document.getElementsByClassName('music').item(la).style.display='block';
   }else{
-    document.getElementsByClassName('music').item(1).style.display='none';
+    document.getElementsByClassName('music').item(la).style.display='none';
   }
-  /*if(sec==3){
-    document.getElementsById('videos').style.display='block';
+  if(sec==3){
+    document.getElementsByClassName('videos').item(la).style.display='block';
   }else{
-    document.getElementsById('videos').style.display='none';
+    document.getElementsByClassName('games').item(la).style.display='none';
   }
   if(sec==4){
-    document.getElementById('games').style.display='block';
+    document.getElementsByClassName('videos').item(la).style.display='block';
   }else{
-    document.getElementById('games').style.display='none';
-  }*/
+    document.getElementsByClassName('games').item(la).style.display='none';
+  }
 }
