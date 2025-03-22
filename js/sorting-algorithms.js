2658256e032360e11abfe0428b3a89627787fc6f
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-03-22 05:08:22 +0000

    Update sorting-algorithms.js

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -4,12 +4,13 @@ function main(){
   const img_pre = new Image();
   img_pre.src = "img/sorting-algorithms/presentation.png";
   img_pre.onload = function() {
-    for(let i=1; i!=0; i-=0.02){
+    for(let i=1; i>0; i-=0.05){
+      ctx.clearRect(0,0,640,480);
+      ctx.globalAlpha = 1;
+      ctx.drawImage(img_pre, 0, 0);
       ctx.globalAlpha = i;
       ctx.fillStyle = "black";
       ctx.fillRect(0, 0, 640, 480);
-      ctx.globalAlpha = 1;
-      ctx.drawImage(img_pre, 0, 0);
     }
   };
 }
