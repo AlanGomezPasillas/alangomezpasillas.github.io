650f91d39b19fb77837818b546232bdc7ab16178
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-03-22 06:35:27 +0000

    checking animation

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -5,18 +5,24 @@ function main(){
   img_pre.src = "img/sorting-algorithms/presentation.png";
   img_pre.onload = function() {
     ctx.fillStyle = "black";
-    setTimeout(fadeIn, 100, 1, ctx, img_pre);
+    setTimeout(fade, 100, 1, ctx, img_pre, 1);
+    setTimeout(fade, 4000, 0, ctx, img_pre, 0);
   };
 }
 
-function fadeIn(i, ctx, img_pre){
+function fade(i, ctx, img_pre, type){
   ctx.clearRect(0,0,640,480);
   ctx.globalAlpha = 1;
   ctx.drawImage(img_pre, 0, 0);
   ctx.globalAlpha = i;
   ctx.fillRect(0, 0, 640, 480);
-  i-=0.05;
-  if(i>0)setTimeout(fadeIn, 100, i, ctx, img_pre);
+  if(type == 1){
+    i-=0.05;
+    if(i>0)setTimeout(fade, 100, i, ctx, img_pre, type);
+  }else{
+    i+=0.05;
+    if(i<1)setTimeout(fade, 100, i, ctx, img_pre, type);
+  }
 }
   
 main();
