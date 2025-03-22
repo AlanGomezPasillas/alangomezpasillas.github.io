efc0ca1b155e845d118a25525bd5bb2315958747
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-03-22 12:41:03 -0600

    testing sprite object

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -1,17 +1,35 @@
+class Sprite {
+  constructor(img, nFrames, width, height, px, py, x, y) {
+    this.img = img;
+    this.nFrames = nFrames;
+    this.width = width;
+    this.height = height;
+    this.px = px;
+    this.py = py;
+    this.x = x;
+    this.y = y;
+  }
+}
+
 function main(){
   var canvas = document.getElementById('game');
   var ctx = canvas.getContext('2d');
   const img_pre = new Image();
   const img_tit = new Image();
+  const img_cli = new Image();
   img_pre.src = "img/sorting-algorithms/presentation.png";
   img_tit.src = "img/sorting-algorithms/title.png";
+  img_cli.src = "img/sorting-algorithms/click.png";
   img_pre.onload = function() {
     ctx.fillStyle = "black";
     setTimeout(fade, 100, 1, ctx, img_pre, 1);
     setTimeout(fade, 4000, 0, ctx, img_pre, 0);
     ctx.globalAlpha = 1;
-    setTimeout(title, 6000, ctx, img_tit);
+    setTimeout(title, 6000, ctx, img_tit, obj_cli);
   };
+  img_cli.onload = function() {
+    const obj_cli = new Sprite(img_cli, 10, 180, 64, 0, 0, 230, 200);
+  }
 }
 
 function fade(i, ctx, img_pre, type){
@@ -29,12 +47,13 @@ function fade(i, ctx, img_pre, type){
   }
 }
 
-function title(ctx, img_tit){
+function title(ctx, img_tit, obj_cli){
   ctx.clearRect(0,0,640,480);
   ctx.drawImage(img_tit, 0, 0);
+  ctx.drawImage(obj_cli.img, obj_cli.px, obj_cli.py, obj_cli.width, obj_cli.height, obj_cli.x, obj_cli.y);
   /*var audio = new Audio('audio_file.mp3');
   audio.play();*/
-  setTimeout(title, 10, ctx, img_tit);
+  setTimeout(title, 10, ctx, img_tit, obj_cli);
 }
 
 main();
