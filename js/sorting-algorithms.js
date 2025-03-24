5a4e30969b6338cda5b6c5f6890de9c8401017a9
Alan <alangomezpasillas@gmail.com>
2025-03-24 00:42:50 +0000

    now everything works asyncronously and title is finished

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -25,7 +25,11 @@ class Sprite {
   }
 }
 
-function main(){
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function main(){
   var canvas = document.getElementById('game');
   var ctx = canvas.getContext('2d');
   const img_pre = new Image();
@@ -36,16 +40,26 @@ function main(){
   img_pre.src = "img/sorting-algorithms/presentation.png";
   img_tit.src = "img/sorting-algorithms/title.png";
   img_cli.src = "img/sorting-algorithms/cli-st.png";
-  img_cli.src = "img/sorting-algorithms/bubble-sort.png";
-  img_cli.src = "img/sorting-algorithms/file-select.png";
-  img_cli.onload = function() {
-    setTimeout(fade, 100, 1, ctx, img_pre, 1);
-    setTimeout(fade, 4000, 0, ctx, img_pre, 0);
-    ctx.globalAlpha = 1;
-    ctx.fillStyle = "white";
-    const obj_cli = new Sprite(img_cli, 0, 0, 180, 64, 230, 200, 10, 0);
-    setTimeout(title, 6000, ctx, img_tit, obj_cli);
-  };
+  img_bubs.src = "img/sorting-algorithms/bubble-sort.png";
+  img_fils.src = "img/sorting-algorithms/file-select.png";
+  //img_cli.onload = function() {
+  //setTimeout(fade, 100, 1, ctx, img_pre, 1);
+  await sleep(100);
+  fade(1, ctx, img_pre, 1);
+  await sleep(4000);
+  fade(0, ctx, img_pre, 0);
+  //setTimeout(fade, 4000, 0, ctx, img_pre, 0);
+  ctx.globalAlpha = 1;
+  ctx.fillStyle = "white";
+  const obj_cli = new Sprite(img_cli, 0, 0, 180, 64, 230, 200, 10, 0);
+  /*let mypromise = new Promise(function(resolve){
+    setTimeout(() => {}, 6100);
+  });*/
+  await sleep(2500);
+  //rInTime("tit", 6100, ctx, img_tit, img_bubs, img_fils, obj_cli);
+  await title(ctx, img_tit, obj_cli);
+  await selectAlg(ctx, img_bubs, img_fils);
+  //};
 }
 
 function fade(i, ctx, img_pre, type){
@@ -64,7 +78,7 @@ function fade(i, ctx, img_pre, type){
   }
 }
 
-function title(ctx, img_tit, obj_cli){
+async function title(ctx, img_tit, obj_cli){
   state = "title";
   var timeout=100;
   ctx.fillStyle = "white";
@@ -81,14 +95,22 @@ function title(ctx, img_tit, obj_cli){
   audio.play();*/
   titY+=titY;
   if(titY < -880){
-    selectAlg();
+    return sleep(0);
+    //return;
   } else {
-    setTimeout(title, timeout, ctx, img_tit, obj_cli);
+    await sleep(timeout);
+    //setTimeout(title, timeout, ctx, img_tit, obj_cli);
+    await title(ctx, img_tit, obj_cli);
   }
 }
 
-function selectAlg(){
+function selectAlg(ctx, img_bubs, img_fils){
   //todo draw images of bubble sort and file select
+  state = "select";
+  ctx.clearRect(0, 0, 640, 480);
+  ctx.fillRect(0, 0, 640, 480);
+  ctx.drawImage(img_bubs, 136, 126);
+  ctx.drawImage(img_fils, 364, 126);
 }
 
 canvas.addEventListener("click", (e) => {
