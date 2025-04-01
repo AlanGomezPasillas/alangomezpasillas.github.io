a8565e3d6cfafe5e3527e370af7343eec137520d
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-04-01 16:19:49 -0600

    testing and fixing

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -205,6 +205,7 @@ async function selectAlg(canvas, ctx, imgBubs, imgFils, imgPlay, n, arrNum, chec
 async function initPlay(imgBub, n, arrNum, arrBub){
   const arrSorted = arrNum.toSorted(function (a, b){return a - b;});
   console.log(n);
+  console.log(arrNum);
   for(let i = 0; i < n; i++){
     const bub = new Bubble(imgBub, 20, 400/n, i*(400/n)+120, (n-arrSorted.indexOf(arrNum[i]))*(400/n)+20, arrNum[i]);
     arrSorted[i]=-123456;
