8984cf8ed15325e55a2f8d132ff572ea3342547f
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-04-18 14:29:21 -0600

    almost done last testings

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -35,7 +35,7 @@ class Bubble {
     if (Math.round(this.x) < Math.round(this.go)) {
       this.x -= velocity;
       if (this.img.src == "https://alangomezpasillas.github.io/img/sorting-algorithms/cube.png") {
-        this.y = Math.cbrt(Math.sin(((Math.PI)*(this.x+1))/this.tdis)*64000)+232;
+        this.y = Math.cbrt(Math.sin(((Math.PI)*(this.x+1))/this.tdis)*64000)+252;
       }
     } else if(Math.round(this.x) > Math.round(this.go)) {
       this.x -= velocity;
