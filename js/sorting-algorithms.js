779b639400d3862f6b4a8a7b209e9d68bb565f9c
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-04-23 16:32:02 -0600

    almost

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -342,7 +342,7 @@ async function initPlay(imgBub, imgBub2, imgForu, imgForu2, imgCube, imgCube2, a
     } else if (c.alg == "select") {
       bub = new Bubble(imgForu, imgForu2, arr.spds[arr.si], sizex, -sizex+arr.n-arrSorted.indexOf(arr.nums[i])*sizex, i*sizex+120, arr.n*sizex+40, arr.nums[i], 0);
     } else if (c.alg == "insert") {
-      bub = new Bubble(imgCube, imgCube2, arr.spds[arr.si], sizex, sizex, i*sizex+100+(sizex*0.11), 228, arr.nums[i], sizex*0.5);
+      bub = new Bubble(imgCube, imgCube2, arr.spds[arr.si], sizex, sizex, i*sizex+100+(sizex*0.1), 228, arr.nums[i], sizex*0.5);
       c.swapped = false;
     }
     if (c.alg != "select") arrSorted[arrSorted.indexOf(arr.nums[i])] = -123456;
