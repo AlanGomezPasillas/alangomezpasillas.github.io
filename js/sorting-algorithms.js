6b2ba84ddc44af97c368b605947abba80c2f27cb
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-04-23 18:28:28 -0600

    still testing

--- js/sorting-algorithms.js
+++ js/sorting-algorithms.js
@@ -38,12 +38,12 @@ class Bubble {
     if (Math.round(this.x) < Math.round(this.go)) {
       this.x -= velocity;
       if (this.img.src == "https://alangomezpasillas.github.io/img/sorting-algorithms/cube.png") {
-	    this.y = -Math.cbrt(Math.pow(Math.sin((Math.PI*(this.x/2))/this.tdis), 2)*64000)+260;
+	    this.y = -Math.cbrt(Math.pow(Math.sin((Math.PI*(this.x/2))/this.tdis), 2)*100000)+260;
       }
     } else if(Math.round(this.x) > Math.round(this.go)) {
       this.x -= velocity;
       if (this.img.src == "https://alangomezpasillas.github.io/img/sorting-algorithms/cube.png") {
-        this.y = Math.cbrt(Math.pow(Math.sin((Math.PI*(this.x/2))/this.tdis), 2)*64000)+192;
+        this.y = Math.cbrt(Math.pow(Math.sin((Math.PI*(this.x/2))/this.tdis), 2)*100000)+192;
       }
     } else {
       this.x = Math.round(this.x);
@@ -342,7 +342,7 @@ async function initPlay(imgBub, imgBub2, imgForu, imgForu2, imgCube, imgCube2, a
     } else if (c.alg == "select") {
       bub = new Bubble(imgForu, imgForu2, arr.spds[arr.si], sizex, -sizex+arr.n-arrSorted.indexOf(arr.nums[i])*sizex, i*sizex+120, arr.n*sizex+40, arr.nums[i], 0);
     } else if (c.alg == "insert") {
-      bub = new Bubble(imgCube, imgCube2, arr.spds[arr.si], sizex, sizex, i*sizex+100+(sizex*0.1), 228, arr.nums[i], sizex*0.5);
+      bub = new Bubble(imgCube, imgCube2, arr.spds[arr.si], sizex, sizex, i*sizex+100+(sizex), 228, arr.nums[i], sizex);
       c.swapped = false;
     }
     if (c.alg != "select") arrSorted[arrSorted.indexOf(arr.nums[i])] = -123456;
