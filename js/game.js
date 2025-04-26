4b45c422d4858469f551dbbd7e569d9769ef63b9
AlanGomezPasillas <alangomezpasillas@gmail.com>
2025-04-25 18:54:34 -0600

    added an enhanced use and optimization

--- js/game.js
+++ js/game.js
@@ -1,22 +1,39 @@
 function loadGame(){
-  changeLanguage(2);
   const game = new URLSearchParams(window.location.search).get('game');
-  const name = getName(game, 0);
-  const name2 = getName(game, 1);
-  const desc = getDesc(game, 0);
-  const desc2 = getName(game, 1);
+  const name = getAttr(game, "name");
+  const desc = getDesc(game, "desc");
   document.querySelector('title').innerText = name;
   document.getElementById('title').innerHTML = 
     `<h2>
       <span class="en">${name}</span>
-      <span class="es">${name2}</span>
+      <span class="es">${name}</span>
     </h2>
     <p>
       <span class="en">
         ${desc}
       </span>
       <span class="es">
-        ${desc2}
+        ${desc}
       </span>
     </p>`;
+}
+
+function getAttr(game, attr) { 
+  switch (game) {
+      case "sorting-algorithms": return sortingAlgorithms(attr);
+  }
+}
+
+function sortingAlgorithms(attr) {
+  const name = ["Sorting Algorithms","Algoritmos de Ordenamiento"];
+  const desc = [`In this game you will be able to choose a sorting algorithm
+          and use it with a set of numbers that can be random or
+          not, then, you will be able to see how it works step by step.`,
+                `En este juego podrás elegir un algoritmo de ordenamiento y
+          usarlo con un conjunto de numeros que puede ser aleatorio o
+          no, después, podras ver como se comporta paso a paso.`];
+  switch (attr){
+    case "name": return name[la];
+    case "desc": return desc[la];
+  }
 }
